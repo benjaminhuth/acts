6222eeabac01675772bd02962feda3d7133ddc22
Benjamin Huth <Benjamin-Huth@t-online.de>
2023-02-20 09:02:04 +0100

    update

--- Core/include/Acts/Utilities/detail/gaussian_mixture_helpers.hpp
+++ Core/include/Acts/Utilities/detail/gaussian_mixture_helpers.hpp
@@ -74,15 +74,29 @@ auto angleDescriptionSwitch(const Surface &surface, Callable &&callable) {
   }
 }
 
+/// Compute the covariance of a Gaussian Mixture. Uses some approximations
+/// for cyclic coordinates (no circular mean).
+///
+/// @param components A range of components
+/// @param mean The mean of the Gaussian Mixture
+/// @param projector A projector, which maps the component to a
+/// std::tuple< weight, mean, cov >
+/// @param angleDesc A angle description object which defines the cyclic
+/// angles in the bound parameters
 template <int D, typename components_t, typename projector_t,
           typename angle_desc_t>
-auto combineCov(const components_t components, const ActsVector<D> &mean,
-                double sumOfWeights, projector_t &&projector,
-                const angle_desc_t &angleDesc) {
+auto gaussianMixtureCovariance(const components_t components,
+                               const ActsVector<D> &mean,
+                               projector_t &&projector,
+                               const angle_desc_t &angleDesc) {
+  // Zero initialized values for aggregation
   ActsSymMatrix<D> cov = ActsSymMatrix<D>::Zero();
+  double sumOfWeights{0.0};
 
   for (const auto &cmp : components) {
     const auto &[weight_l, pars_l, cov_l] = projector(cmp);
+
+    sumOfWeights += weight_l;
 
     cov += weight_l * cov_l;
 
@@ -105,63 +119,25 @@ auto combineCov(const components_t components, const ActsVector<D> &mean,
   return cov;
 }
 
-/// @brief Combine multiple components into one representative track state
-/// object. The function takes iterators to allow for arbitrary ranges to be
-/// combined. The dimension of the vectors is infeared from the inputs.
+/// Compute the mean of a Gaussian Mixture. Uses some approximations for
+/// cyclic coordinates (no circular mean).
 ///
-/// @note If one component does not contain a covariance, no covariance is
-/// computed.
-///
-/// @note The correct mean and variances for cyclic coordnates or spherical
-/// coordinates (theta, phi) must generally be computed using a special circular
-/// mean or in cartesian coordinates. This implements a approximation, which
-/// only works well for close components.
-///
-/// @tparam components_t A range of components
-/// @tparam projector_t A projector, which maps the component to a
-/// std::tuple< weight, mean, std::optional< cov > >
-/// @tparam angle_desc_t A angle description object which defines the cyclic
+/// @param components A range of components
+/// @param projector A projector, which maps the component to a
+/// std::tuple< weight, mean, cov >
+/// @param angleDesc A angle description object which defines the cyclic
 /// angles in the bound parameters
-template <typename components_t, typename projector_t = Identity,
-          typename angle_desc_t = AngleDescription<Surface::Plane>::Desc>
-auto combineGaussianMixture(const components_t components,
-                            projector_t &&projector = projector_t{},
-                            const angle_desc_t &angleDesc = angle_desc_t{}) {
+template <int D, typename components_t, typename projector_t,
+          typename angle_desc_t>
+auto gaussianMixtureMean(const components_t components, projector_t &&projector,
+                         const angle_desc_t &angleDesc) {
   // Extract the first component
   const auto &[beginWeight, beginPars, beginCov] =
       projector(components.front());
 
-  // Assert type properties
-  using ParsType = std::decay_t<decltype(beginPars)>;
-  using CovType = std::decay_t<decltype(beginCov)>;
-  using WeightType = std::decay_t<decltype(beginWeight)>;
-
-  constexpr int D = ParsType::RowsAtCompileTime;
-  EIGEN_STATIC_ASSERT_VECTOR_ONLY(ParsType);
-  EIGEN_STATIC_ASSERT_MATRIX_SPECIFIC_SIZE(CovType, D, D);
-  static_assert(std::is_floating_point_v<WeightType>);
-
-  // gcc 8 does not like this statement somehow. We must handle clang here since
-  // it defines __GNUC__ as 4.
-#if defined(__GNUC__) && __GNUC__ < 9 && !defined(__clang__)
-  // No check
-#else
-  std::apply(
-      [&](auto... d) { static_assert((std::less<int>{}(d.idx, D) && ...)); },
-      angleDesc);
-#endif
-
-  // Define the return type
-  using RetType = std::tuple<ActsVector<D>, ActsSymMatrix<D>>;
-
-  // Early return in case of range with length 1
-  if (components.size() == 1) {
-    return RetType{beginPars / beginWeight, beginCov / beginWeight};
-  }
-
   // Zero initialized values for aggregation
   ActsVector<D> mean = ActsVector<D>::Zero();
-  WeightType sumOfWeights{0.0};
+  double sumOfWeights{0.0};
 
   for (const auto &cmp : components) {
     const auto &[weight_l, pars_l, cov_l] = projector(cmp);
@@ -194,8 +170,63 @@ auto combineGaussianMixture(const components_t components,
 
   std::apply([&](auto... dsc) { (wrap(dsc), ...); }, angleDesc);
 
+  return mean;
+}
+
+/// @brief Combine multiple components into one representative track state
+/// object. The function takes iterators to allow for arbitrary ranges to be
+/// combined. The dimension of the vectors is infeared from the inputs.
+///
+/// @note The correct mean and variances for cyclic coordnates or spherical
+/// coordinates (theta, phi) must generally be computed using a special circular
+/// mean or in cartesian coordinates. This implements a approximation, which
+/// only works well for close components.
+///
+/// @tparam components_t A range of components
+/// @tparam projector_t A projector, which maps the component to a
+/// std::tuple< weight, mean, cov >
+/// @tparam angle_desc_t A angle description object which defines the cyclic
+/// angles in the bound parameters
+template <typename components_t, typename projector_t = Identity,
+          typename angle_desc_t = AngleDescription<Surface::Plane>::Desc>
+auto combineGaussianMixture(const components_t components,
+                            projector_t &&projector = projector_t{},
+                            const angle_desc_t &angleDesc = angle_desc_t{}) {
+  // Extract the first component
+  const auto &[beginWeight, beginPars, beginCov] =
+      projector(components.front());
+
+  // Assert type properties
+  using ParsType = std::decay_t<decltype(beginPars)>;
+  using CovType = std::decay_t<decltype(beginCov)>;
+  using WeightType = std::decay_t<decltype(beginWeight)>;
+
+  constexpr int D = ParsType::RowsAtCompileTime;
+  EIGEN_STATIC_ASSERT_VECTOR_ONLY(ParsType);
+  EIGEN_STATIC_ASSERT_MATRIX_SPECIFIC_SIZE(CovType, D, D);
+  static_assert(std::is_floating_point_v<WeightType>);
+
+  // Define the return type
+  using RetType = std::tuple<ActsVector<D>, ActsSymMatrix<D>>;
+
+  // Early return in case of range with length 1
+  if (components.size() == 1) {
+    return RetType{beginPars / beginWeight, beginCov / beginWeight};
+  }
+
+  // gcc 8 does not like this statement somehow. We must handle clang here since
+  // it defines __GNUC__ as 4.
+#if defined(__GNUC__) && __GNUC__ < 9 && !defined(__clang__)
+  // No check
+#else
+  std::apply(
+      [&](auto... d) { static_assert((std::less<int>{}(d.idx, D) && ...)); },
+      angleDesc);
+#endif
+
+  const auto mean = gaussianMixtureMean<D>(components, projector, angleDesc);
   const auto cov =
-      combineCov(components, mean, sumOfWeights, projector, angleDesc);
+      gaussianMixtureCovariance<D>(components, mean, projector, angleDesc);
 
   return RetType{mean, cov};
 }
